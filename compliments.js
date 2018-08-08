44832e2e3691faed2ef6628e30a7ef99cfc02ae0
PineapplesOfJustice <gre8nthu7der@gmail.com>
2018-08-07 17:55:04 -0700

    updated image system

--- compliments.js
+++ compliments.js
@@ -1,41 +1,41 @@
 
-var photos = ["http://sweetdessertcafe.com/uploads/9/1/9/3/91939434/pancakes-sweet-dessert-cafe.jpg?284",
-             "http://s1.1zoom.me/big0/253/344721-admin.jpg",
-             "http://files.kitchenbowl.com/recipe/luDbDIvw03/chinese-sweet-soup-dessert.jpg",
-             "http://del.h-cdn.co/assets/16/07/1455890695-chocolate-banana-donuts.jpg",
-             "https://images-gmi-pmc.edge-generalmills.com/fb78ee4c-fda4-42c5-bc2b-a3ecf0096a29.jpg",
-             "https://www.lifeloveandsugar.com/wp-content/uploads/2014/04/Perfect_Moist_Fluffy_Vanilla_Cupcakes8.jpg",
-             "http://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/9/28/0/FNK_Waffled-Brownie-Sundae_s4x3.jpg.rend.hgtvcom.616.462.suffix/1383787370815.jpeg",
-             "https://images-gmi-pmc.edge-generalmills.com/986c471f-40ff-466d-850a-1c0f9295439f.jpg",
-             "https://www.thecheesecakefactory.com/assets/images/Menu-Import/CCF_FreshStrawberryCheesecake.jpg",
-             "http://farm8.staticflickr.com/7284/9076471099_40aafe6182_c.jpg",
-             "https://i.ytimg.com/vi/OECPo018MUs/maxresdefault.jpg",
-             "http://cachlambanhflan.net/wp-content/uploads/2017/03/nguyen-nhan-lam-banh-flan-bi-ro-va-cach-khac-phuc-nhu-the-nao.jpg",
-"https://static1.squarespace.com/static/5536ab39e4b011692d3b000f/55396b6be4b02fb0125a0416/55398189e4b040d51046ae25/1430112920128/IMG_3277%282%29.jpg",
-             "https://i.ytimg.com/vi/gEWrtGQ7qHk/maxresdefault.jpg",
-             "http://www.tastehongkong.com/wp/2011/white-fungus-papaya-a.jpg",
-             "https://media1.fdncms.com/sacurrent/imager/u/original/3034615/17156039_1843135842608739_5971395632376007995_n.jpg",
-             "https://assets.epicurious.com/photos/57990894bd7fde737c83776e/master/pass/breakfast-crepes.jpg",
-             "http://food.fnr.sndimg.com/content/dam/images/food/fullset/2008/11/24/0/1G1C11_Creme-Brulee.jpg.rend.hgtvcom.406.305.suffix/1382538717232.jpeg", ];
+var photos = ["Pancake.jpg",
+              "Cake.jpg",
+              "Soup.jpg",
+              "Donut.jpg",
+              "Pie.jpg",
+              "Cupcake.jpg",
+              "Sundae.jpeg",
+              "Churros.jpg",
+              "Cheesecake.jpg",
+              "Yogurt.jpg",
+              "Cassava.jpg",
+              "Flan.jpg",
+              "Shaved Ice.jpg",
+              "Bun.jpg",
+              "Fungus.jpg",
+              "Pan Dulce.jpg",
+              "Crepe.jpg",
+              "Creme Brulee.jpeg", ];
 
 var dessertList = ["pancake",
                    "cake",
-                  "soup",
-                  "donut",
-                  "pie",
-                  "cupcake",
-                  "sundae",
-                  "churros",
-                  "cheesecake",
-                  "yogurt",
-                  "cassava",
-                  "flan",
-                  "shaved ice",
-                  "bun",
-                  "fungus",
-                  "pan dulce",
-                  "crepe",
-                  "creme brulee", ];
+                   "soup",
+                   "donut",
+                   "pie",
+                   "cupcake",
+                   "sundae",
+                   "churros",
+                   "cheesecake",
+                   "yogurt",
+                   "cassava",
+                   "flan",
+                   "shaved ice",
+                   "bun",
+                   "fungus",
+                   "pan dulce",
+                   "crepe",
+                   "creme brulee", ];
 
 var adjectiveList = ["lip-smacking",
                      "sweet",
@@ -96,12 +96,12 @@ function complimentTime(){
   decideAdjective1();
   decideAdjective2();
   if(generator == "compliment"){  
-    document.getElementById("imageContainer").innerHTML = "<img src='" + photos[dessert] + "' height='100px' alt='" + dessertList[dessert] + "'>";
+    document.getElementById("imageContainer").innerHTML = "<img src='Images/Food/" + photos[dessert] + "' height='100px' alt='" + dessertList[dessert] + "'>";
     document.getElementById("textArea").innerHTML = "I know your true identity " + name + "! <br> You " + adjectiveList[adjective1] + ", " + adjectiveList[adjective2] + " " + dessertList[dessert] + "!";
     console.log("   - " + name + ", friend of " + adjectiveList[adjective1] + ", " + adjectiveList[adjective2] + " " + dessertList[dessert] + "!");  
   }
   else if(generator == "insult"){  
-    document.getElementById("imageContainer").innerHTML = "<img src='" + photos[dessert] + "' height='100px' alt='" + dessertList[dessert] + "'>";
+    document.getElementById("imageContainer").innerHTML = "<img src='Images/Food/" + photos[dessert] + "' height='100px' alt='" + dessertList[dessert] + "'>";
     document.getElementById("textArea").innerHTML = "I know your true identity " + name + "! <br> Enemy of defenseless " + adjectiveList[adjective1] + ", " + adjectiveList[adjective2] + " " + dessertList[dessert] + "s!";
     console.log("   - " + name + ", enemy of " + adjectiveList[adjective1] + ", " + adjectiveList[adjective2] + " " + dessertList[dessert] + "!");  
   }
